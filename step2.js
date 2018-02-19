cf6034425de0a2ae41b8961469249b828125971b
tinanwlin <tinanwlin@gmail.com>
2018-02-19 22:26:03 +0000

    Complete step1 getAndPrintHTML function.

---
+++ step2.js
@@ -0,0 +1,23 @@
+var https = require('https');
+
+function getAndPrintHTML () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function(response){
+      response.setEncoding('utf8');
+      response.on('data', function(data){
+          var result = "";
+          result += data + "\n";
+          console.log("Result received: ", result);
+      })
+      response.on('end', function(){
+          console.log('Response stream complete at step2.');
+      })
+  })
+}
+
+getAndPrintHTML();
